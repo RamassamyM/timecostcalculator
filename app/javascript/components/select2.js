e599aac137896156fb6984860b71623042e1cb7b
Michael Ramassamy <michaelr@MacBook-Pro-de-Michael.local>
2021-03-17 19:47:36 +0100

    Implement and enhance css form section purchase6

--- app/javascript/components/select2.js
+++ app/javascript/components/select2.js
@@ -2,7 +2,17 @@ import $ from 'jquery';
 import 'select2';
 
 const initSelect2 = () => {
-  $('.select2').select2();
+  jQuery(function() {
+    $('.select2').select2({
+      maximumSelectionLength: 3,
+    });
+    $('.select2').val('');
+    $('.select2').trigger('change');
+    $('.select2_for_place_of_delivery').select2({
+      disabled: true,
+      maximumSelectionLength: 3,
+    });
+  })
 };
 
 export { initSelect2 };
