88d8e6b5b380f7ea54ab4ba68db051d072ca81cb
Michael Ramassamy <michaelr@MacBook-Pro-de-Michael.local>
2021-05-29 14:57:44 +0200

    Update dynamic text on banner home

--- app/javascript/components/banner.js
+++ app/javascript/components/banner.js
@@ -3,7 +3,7 @@ import Typed from 'typed.js';
 const loadDynamicBannerText = () => {
   if (window.location.pathname === '/') {
     new Typed('#banner-typed-text', {
-      strings: ["Calculate cost and time", "Optimize shipping purchases"],
+      strings: ["Calculate Shipping Costs.", "Estimate Transit Times."],
       typeSpeed: 40,
       loop: true
     });
